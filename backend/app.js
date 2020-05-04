42323c9864a0123676190bb916abb3ae194f29f2
shayanmalinda <shayanmalinda@gmail.com>
2020-05-04 21:52:52 +0530

    changes in folder structure, mongodb atlas

---
+++ backend/app.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+var cors = require("cors");
+const dotenv = require("dotenv").config();
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+const URL = process.env.MONGODB_URI;
+
+mongoose
+  .connect(URL, {
+    dbName: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+  })
+  .then(() => {
+    console.log("mongoDB Connected ...");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected to db ....");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("Error: ", err.message);
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoose connection is disconnected ....");
+});
+
+process.on("SIGNIT", () => {
+  mongoose.connection.close(() => {
+    console.log("mongoose connection is terminated due to app termination");
+    process.exit(0);
+  });
+});
+
+// app.use(passport.initialize());
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const users = require("./routes/users");
+app.use("/users", users);
+
+app.get("/", function (req, res) {
+  res.send("Hello world");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, function () {
+  console.log("listening to port " + PORT);
+});
+
+module.exports = app;
---
+++ backend/models/users.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.schema;
+
+var userSchema = mongoose.Schema({
+  firstName: { type: String },
+  lastName: { type: String },
+  email: { type: String },
+  password: { type: String },
+  birthday: { type: String },
+  nic: { type: String },
+  mobile: { type: String },
+  isDeleted: { type: Boolean },
+});
+
+const Users = (module.exports = mongoose.model("Users", userSchema));
