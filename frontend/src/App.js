ba6a7d75394ac16e4a076ffa88bf73e3dc985aac
Sachin Muthumala <43057523+sachintsm@users.noreply.github.com>
2020-05-06 00:08:46 +0530

    Correcting Errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,14 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Navbar from "./components/shared/Navbar";
-import Footer from "./components/shared/Footer";
 
 import Registration from "./components/Registration";
 import Login from "./components/Login";
-import Home from "./components/basic/Home";
 import AdminHome from "./components/admin/AdminHome";
 import CoordinatorHome from "./components/coordinator/CoordinatorHome";
 import SupervisorHome from "./components/supervisor/SupervisorHome";
 import StudentHome from "./components/student/StudentHome";
+
 require("dotenv").config();
 
 function App() {
