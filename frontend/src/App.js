001c0a5eeeb6ce47fed0301ac922823e04e04d0c
Sachin Muthumala <43057523+sachintsm@users.noreply.github.com>
2020-05-05 22:04:24 +0530

    FIx folder names

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Navbar from "./Components/auth/navbar";
-import Footer from "./Components/auth/footer";
+import Navbar from "./components/auth/navbar";
+import Footer from "./components/auth/footer";
 
-import Registration from "./Components/admin/registration";
-import Login from "./Components/admin/login";
-import Home from "./Components/basic/Home";
+import Registration from "./components/admin/registration";
+import Login from "./components/admin/login";
+import Home from "./components/basic/home";
 
 function App() {
   return (
