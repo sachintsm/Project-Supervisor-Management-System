8b2e560ac5e4675986a5a1270d5d9f57203fc366
Sachin Muthumala <43057523+sachintsm@users.noreply.github.com>
2020-05-06 14:41:49 +0530

    x

--- frontend/src/Components/student/StudentHome.js
+++ frontend/src/Components/student/StudentHome.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { verifyAuth } from "../../utils/authentication";
+import { verifyAuth } from "../../utils/Authentication";
 import Navbar from "../shared/Navbar";
 
 class StudentHome extends Component {
@@ -11,7 +11,7 @@ class StudentHome extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar />
+        <Navbar panel={"student"} />
         <h1>Student Home</h1>
       </React.Fragment>
     );
