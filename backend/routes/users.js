dea9e81fda55d86f45d860a420f553e752fcddcf
shayanmalinda <shayanmalinda@gmail.com>
2020-06-02 22:02:19 +0530

    removed console.log in backend

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -206,7 +206,6 @@ router.post('/bulkRegister', async (req, res, next) => {
 
 //User Login
 router.post('/login', async function (req, res) {
-  console.log('ddddddddddddddddddddddddddddddddddd')
   const password = req.body.password;
   console.log(req.body)
   //checking if the userId is already in the database
