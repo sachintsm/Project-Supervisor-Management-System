2e06c3b5e495cb7db7add99a7710d52711d67f89
shayanmalinda <shayanmalinda@gmail.com>
2020-06-03 21:09:36 +0530

    notice delete snackbar fix

--- frontend/src/components/shared/Notice.js
+++ frontend/src/components/shared/Notice.js
@@ -227,7 +227,7 @@ class Notice extends Component {
   closeAlert = () => {
     this.setState({
       succesAlert: false,
-      warnAlert: false,
+      deleteSuccesAlert: false,
     });
   };
 
