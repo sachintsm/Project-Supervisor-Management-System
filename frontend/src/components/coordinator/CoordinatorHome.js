ca48fc76f5692f20ad3a4623df712b9c9f12d3cc
Sachin Muthumala <43057523+sachintsm@users.noreply.github.com>
2020-06-09 23:24:35 +0530

    UI design on Coordinator Home

--- frontend/src/components/coordinator/CoordinatorHome.js
+++ frontend/src/components/coordinator/CoordinatorHome.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { verifyAuth } from "../../utils/Authentication";
 import Navbar from "../shared/Navbar";
-
+import '../../css/coordinator/CoordinatorHome.css'
+import { Row, Col, Button } from 'reactstrap';
 class CoordinatorHome extends Component {
   componentDidMount = async () => {
-    localStorage.setItem("user-level","coordinator")
+    localStorage.setItem("user-level", "coordinator")
 
     const authState = await verifyAuth();
     this.setState({ authState: authState });
@@ -16,7 +17,43 @@ class CoordinatorHome extends Component {
     return (
       <React.Fragment>
         <Navbar panel={"coordinator"} />
-        <h1>Coordinator Home</h1>
+        <div className="container">
+          <div className="ch-topic-div">
+            <p className="ch-topic">On-going Projects</p>
+          </div>
+
+          <div className="card container">
+            <Row className="ch-project-name-div">
+              <p className="ch-project-name">Project Name</p>
+            </Row>
+            <Row className="">
+              <p className="">Project Name</p>
+            </Row>
+            <Row className="container ch-btn-row">
+              <Col md={2}>
+                <Button className="btn btn-info ch-btn-btn">Supervisors</Button>
+              </Col>
+              <Col md={2}>
+                <Button className="btn btn-info ch-btn-btn">Groups</Button>
+              </Col>
+              <Col md={2}>
+                <Button className="btn btn-info ch-btn-btn">Bi-Weekly</Button>
+              </Col>
+              <Col md={2}>
+                <Button className="btn btn-info ch-btn-btn">SRC Documents</Button>
+              </Col>
+              <Col md={2}>
+                <Button className="btn btn-info ch-btn-btn">Proposals</Button>
+              </Col>
+
+            </Row>
+          </div>
+
+          <div className="ch-topic-div">
+            <p className="ch-topic">On-going Projects</p>
+          </div>
+
+        </div>
       </React.Fragment>
     );
   }
