459adf68636ff2ce054edadf17f6c8ddd45f8189
Ashan Rajapaksha <43042514+ashangrajapaksha@users.noreply.github.com>
2020-07-07 20:41:33 +0530

    proposel implement start

--- frontend/src/components/coordinator/ProjectData/Proposals.js
+++ frontend/src/components/coordinator/ProjectData/Proposals.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 
 import Navbar from '../../shared/Navbar';
+import Footer from '../../shared/Footer';
 
+import '../../../css/coordinator/proposel.scss';
+
+import {
+    Button,
+    Container,
+    Col,
+    Row,
+    FormControl,
+    FormGroup,
+  } from "react-bootstrap";
 
 
 
@@ -23,11 +36,66 @@ class Proposals extends Component {
         return (
             <React.Fragment>
             <Navbar panel={"coordinator"} />
+
+            <div className="proposel-style">
+            <div className="container-fluid pro-back">
             <div className="container">
-                <div className="card">
-                 <h2>Proposel</h2>
-                </div>
+
+            <Tabs defaultActiveKey="Submited Proposel" id="uncontrolled-tab-example"  className="pro-tabs" style={{marginBottom:"30px"}}>
+
+            <Tab eventKey="Submited Proposel" title="Submited Proposel" className="pro-tab">
+
+            <div className="card conatiner" style={{ margin: "0px 0px 20px 0px" }}>
+
+            <div className="pro-head">
+            <p className="pro-grp-name">G-08</p>
             </div>
+
+            <div className="pro-body">
+                <h6>Proposel Details</h6>
+                <a>Attachment </a>
+            </div>
+
+            </div>
+            
+            </Tab>
+
+
+            <Tab eventKey="Create Link" title="Create Proposel Link" className="pro-tab">
+
+            <div className="card container pro-link-crd">
+
+            <form>
+
+            <Row>
+            <p className="pro-link-name">Crate New Proposel</p>
+            </Row>
+
+            <div class="form-group pro-form">
+            <label for="exampleInputProposel">Proposel Tittle :</label>
+            <input type="text" class="form-control" id="exampleInputProposel" placeholder="Proposel Tittle"/>
+            </div>
+
+            <div class="form-group pro-form">
+            <label for="exampleInputDate">Dead Line :</label>
+            <input type="date" class="form-control" id="exampleInputDate"/>
+            </div>
+
+
+            
+            
+            </form>
+            
+            </div>
+            
+            
+            </Tab>
+            </Tabs>
+            </div>
+            </div>
+            </div>
+
+            <Footer />
            </React.Fragment>
         );
     }
