acb27da847861e4c8edfa44439fef5fc3f4a7af9
Ashan Rajapaksha <43042514+ashangrajapaksha@users.noreply.github.com>
2020-10-07 20:31:19 +0530

    check github

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,7 +27,8 @@ import SupervisorData from './components/coordinator/SupervisorData/SupervisorDa
 import BiWeekly from './components/coordinator/ProjectData/BiWeekly';
 import Groups from './components/coordinator/ProjectData/Groups';
 import Submission from './components/coordinator/ProjectData/Submission';
-import ViewSubmission from './components/coordinator/ProjectData/ViewSubmission'
+import ViewSubmission from './components/coordinator/ProjectData/ViewSubmission';
+//import ViewBiweekly from './components/coordinator/projectData/ViewBiweekly';
 import SRS from './components/coordinator/ProjectData/SRS';
 import Supervisors from './components/coordinator/ProjectData/Supervisors';
 import GroupBiWeekly from './components/coordinator/GroupData/GroupSubmissions/BiWeekly';
@@ -97,6 +98,7 @@ function App() {
           <Route path='/coordinatorhome/projectdata/Groups/:id' component={Groups}></Route>
           <Route path='/coordinatorhome/projectdata/submission/:id' component={Submission}></Route>
           <Route path='/coordinatorhome/projectdata/viewsubmission/:id' component={ViewSubmission}></Route>
+          
 
           <Route path='/coordinatorhome/projectdata/SRS/:id' component={SRS}></Route>
           <Route path='/coordinatorhome/projectdata/Supervisors/:id' component={Supervisors}></Route>
