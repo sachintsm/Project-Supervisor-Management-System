7a689b0bb330474280790ec5cff0c9661e5829f7
shayanmalinda <shayanmalinda@gmail.com>
2020-10-07 14:41:02 +0530

    Accept/Decline biweekly reports (Attachment Not included)

--- frontend/src/components/supervisor/Notifications/SupervisorNotifications.js
+++ frontend/src/components/supervisor/Notifications/SupervisorNotifications.js
@@ -6,8 +6,8 @@ import { Row, Col, Card,Spinner, Container, Button } from 'react-bootstrap';
 import axios from "axios";
 import Snackpop from "../../shared/Snackpop";
 import RequestInfo from "../../student/formgroups/RequestInfo";
-import RequestDetails from "./RequestDetails";
 import { confirmAlert } from 'react-confirm-alert';
+import BiweekRequest from "./BiweekRequest";
 
 const backendURI = require("../../shared/BackendURI");
 
@@ -17,115 +17,110 @@ class SupervisorNotifications extends Component {
         super(props);
         this.state = {
             loading: true,
+            biWeekRequests: [],
             declinedAlert: false,
-            acceptedAlert: false
+            acceptedAlert: false,
+            userId: getFromStorage('auth-id').id
         }
     }
 
     componentDidMount() {
-        // this.getGroupRequestNotification()
+        this.getBiweekRequests()
     }
 
-    // getGroupRequestNotification = () => {
-    //     const headers = {
-    //         'auth-token':getFromStorage('auth-token').token,
-    //     }
-    //     const userId = getFromStorage("auth-id").id
-    //
-    //     axios.get(backendURI.url+"/createGroups/coordinatorgrouprequests/"+userId, {headers: headers}).then(res=>{
-    //         this.setState({
-    //             groupRequests: res.data,
-    //             loading: false
-    //         })
-    //     })
-    // }
+    getBiweekRequests = () => {
 
-    // acceptRequest = (item) => {
-    //
-    //     confirmAlert({
-    //         title: 'Group Request',
-    //         message: 'Are you sure you want to accept this request?',
-    //         buttons: [
-    //             {
-    //                 label: 'Yes',
-    //                 onClick: async () => {
-    //
-    //                     const headers = {
-    //                         'auth-token':getFromStorage('auth-token').token,
-    //                     }
-    //                     const data = {
-    //                         projectId: item.projectId,
-    //                         groupMembers: item.allStudentList
-    //                     }
-    //
-    //                     axios.post(backendURI.url+"/createGroups/add/",data, {headers: headers}).then(res=>{
-    //                         const requestId = item._id
-    //
-    //                         axios.delete(backendURI.url+"/createGroups/grouprequests/"+requestId, {headers:headers}).then(res=>{
-    //                             this.setState({
-    //                                 acceptedAlert: true
-    //                             },()=>{
-    //                                 this.getGroupRequestNotification()
-    //                             })
-    //
-    //                         })
-    //                     })
-    //
-    //
-    //                 }
-    //             },
-    //             {
-    //                 label: 'No',
-    //                 onClick: () => {
-    //
-    //                 }
-    //             }
-    //         ]
-    //     })
-    //
-    //
-    //
-    //
-    // }
+        const headers = {
+            'auth-token':getFromStorage('auth-token').token,
+        }
+        const userId = getFromStorage("auth-id").id
 
-    // declineRequest = (item) => {
-    //
-    //     confirmAlert({
-    //         title: 'Group Request',
-    //         message: 'Are you sure you want to decline this request?',
-    //         buttons: [
-    //             {
-    //                 label: 'Yes',
-    //                 onClick: async () => {
-    //
-    //                     const requestId = item._id
-    //                     const headers = {
-    //                         'auth-token':getFromStorage('auth-token').token,
-    //                     }
-    //
-    //                     axios.delete(backendURI.url+"/createGroups/grouprequests/"+requestId, {headers:headers}).then(res=>{
-    //                         this.setState({
-    //                             declinedAlert: true
-    //                         },()=>{
-    //                             this.getGroupRequestNotification()
-    //                         })
-    //
-    //                     })
-    //
-    //                 }
-    //             },
-    //             {
-    //                 label: 'No',
-    //                 onClick: () => {
-    //
-    //                 }
-    //             }
-    //         ]
-    //     })
-    //
-    //
-    //
-    // }
+        axios.get(backendURI.url+"/biweeksubmissions/getsubmissions/"+userId, {headers: headers}).then(res=>{
+            this.setState({
+                biWeekRequests: res.data,
+                loading: false
+            })
+        })
+    }
+
+
+    acceptRequest = (item) => {
+        confirmAlert({
+            title: 'Biweekly Report',
+            message: 'Are you sure you want to accept this report?',
+            buttons: [
+                {
+                    label: 'Yes',
+                    onClick: async () => {
+
+                        const headers = {
+                            'auth-token':getFromStorage('auth-token').token,
+                        }
+
+                        item.supervisors.map((supervisor,index) => {
+                            if(supervisor === this.state.userId ){
+                                item.status[index]="Accepted"
+                            }
+                        })
+
+
+                        axios.patch(backendURI.url+"/biweeksubmissions/updateRequest/"+item._id,item, {headers: headers}).then(res=>{
+                            this.getBiweekRequests()
+                            this.setState({
+                                acceptedAlert: true
+                            })
+                        })
+                    }
+                },
+                {
+                    label: 'No',
+                    onClick: () => {
+
+                    }
+                }
+            ]
+        })
+    }
+
+
+    declineRequest = (item) => {
+        confirmAlert({
+            title: 'Biweekly Report',
+            message: 'Are you sure you want to decline this report?',
+            buttons: [
+                {
+                    label: 'Yes',
+                    onClick: async () => {
+
+                        const headers = {
+                            'auth-token':getFromStorage('auth-token').token,
+                        }
+
+                        item.supervisors.map((supervisor,index) => {
+                            if(supervisor === this.state.userId ){
+                                item.status[index]="Declined"
+                            }
+                        })
+
+
+                        axios.patch(backendURI.url+"/biweeksubmissions/updateRequest/"+item._id,item, {headers: headers}).then(res=>{
+                            this.getBiweekRequests()
+                            this.setState({
+                                declinedAlert: true
+                            })
+                        })
+                    }
+                },
+                {
+                    label: 'No',
+                    onClick: () => {
+
+                    }
+                }
+            ]
+        })
+    }
+
 
     closeAlert = () => {
         this.setState({
@@ -135,6 +130,7 @@ class SupervisorNotifications extends Component {
     };
 
     render() {
+
         return (
             <React.Fragment>
 
@@ -156,44 +152,44 @@ class SupervisorNotifications extends Component {
 
                 <Navbar panel={"supervisor"} />
 
-                {/*<div className="container-fluid group-notifications">*/}
-                {/*    <div className="title-div">*/}
-                {/*        <h3 className="title">Notifications </h3>*/}
-                {/*    </div>*/}
-                {/*    <Container>*/}
-                {/*        {this.state.loading &&*/}
-                {/*        <div style={{textAlign:'center', paddingTop: "30px"}}>*/}
-                {/*            <Spinner animation="border" className="spinner" style={{alignContent:'center'}}/>*/}
-                {/*        </div>}*/}
-                {/*        {!this.state.loading && (*/}
+                <div className="container-fluid group-notifications">
+                    <div className="title-div">
+                        <h3 className="title">Notifications </h3>
+                    </div>
+                    <Container>
+                        {this.state.loading &&
+                        <div style={{textAlign:'center', paddingTop: "30px"}}>
+                            <Spinner animation="border" className="spinner" style={{alignContent:'center'}}/>
+                        </div>}
+                        {!this.state.loading && (
 
-                {/*            <div className="card card-div">*/}
+                            <div className="card card-div">
 
-                {/*                {this.state.groupRequests.length===0 && <div style={{textAlign: "center", padding: "20px 0px"}}>No Any Notifications</div>}*/}
+                                {this.state.biWeekRequests.length===0 && <div style={{textAlign: "center", padding: "20px 0px"}}>No Any Notifications</div>}
 
-                {/*                /!*Group Requests Notifications*!/*/}
-                {/*                {this.state.groupRequests.map((item,key)=>{*/}
-                {/*                    return <Card className="notification-card" key={key}>*/}
-                {/*                        <Card.Body className="card-body">*/}
-                {/*                            <Row className="details-row">*/}
-                {/*                                <Col>*/}
-                {/*                                    <h5>New Group Request</h5>*/}
-                {/*                                    /!*<RequestDetails item={item}/>*!/*/}
+                                {/*Group Requests Notifications*/}
+                                {this.state.biWeekRequests.map((item,key)=>{
+                                    return <Card className="notification-card" key={key}>
+                                        <Card.Body className="card-body">
+                                            <Row className="details-row">
+                                                <Col>
+                                                    <h5>New Biweekly Report Request</h5>
+                                                    <BiweekRequest details={item}/>
 
-                {/*                                </Col>*/}
-                {/*                                <Col lg={3} md={3} className="btn-col">*/}
-                {/*                                    <Button className="btn btn-info my-btn1" onClick={()=>this.acceptRequest(item)}>Accept</Button>*/}
-                {/*                                    <Button className="btn btn-danger my-btn1 mt-2" onClick={()=>this.declineRequest(item)} >Decline</Button>*/}
-                {/*                                </Col>*/}
-                {/*                            </Row>*/}
-                {/*                        </Card.Body>*/}
-                {/*                    </Card>*/}
-                {/*                })}*/}
+                                                </Col>
+                                                <Col lg={3} md={3} className="btn-col-2">
+                                                    <Button className="btn btn-info my-btn1" onClick={()=>this.acceptRequest(item)}>Accept Report</Button>
+                                                    <Button className="btn btn-danger my-btn1 mt-3" onClick={()=>this.declineRequest(item)} >Decline Report</Button>
+                                                </Col>
+                                            </Row>
+                                        </Card.Body>
+                                    </Card>
+                                })}
 
-                {/*            </div>*/}
-                {/*        )}*/}
-                {/*    </Container>*/}
-                {/*</div>*/}
+                            </div>
+                        )}
+                    </Container>
+                </div>
 
 
                 <Footer/>
