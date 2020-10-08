9203c26083a5473c0990f94933794e283d3d4f7a
kabsgithub <kabskumaranayake@gmail.com>
2020-10-08 18:08:04 +0530

    Update view meetings by student

--- frontend/src/components/student/ViewMeetBlock.js
+++ frontend/src/components/student/ViewMeetBlock.js
@@ -1,53 +1,78 @@
-import React, { Component } from 'react';
-import { Col } from 'reactstrap'
+import React, { Component } from "react";
+
+import { Col } from "reactstrap";
+
 import { Card } from "react-bootstrap";
-import '../../css/students/ViewMeeting.scss';
+
+import "../../css/students/ViewMeeting.scss";
+
+import { Table } from "react-bootstrap";
 
 class viewMeetBlock extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            meetData: this.props.data
-        }
-        console.log(this.state.meetData);
-    }
-    render() {
-        const { meetData } = this.state;
-        let supervisorProjectList = meetData.length > 0
-            && meetData.map((item, i) => {
+    this.state = {
+      meetData: this.props.data,
+    };
+  }
+
+  render() {
+    const { meetData } = this.state;
+
+    return (
+      <div style={{ marginTop: "20px" }}>
+        <Table hover className="as-table">
+          <thead>
+            <tr>
+              <th className="table-head">Supervisor</th>
+
+              <th className="table-head">Date</th>
+
+              <th className="table-head">Time</th>
+
+              <th className="table-head">Purpose</th>
+
+              <th className="table-head">State</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {meetData.length > 0 &&
+              meetData.map((item, i) => {
                 return (
-                    <div className="container" key={item.id}>
-                        <Col md={12} xs={12} sm={12} >
-                            <div className="view-meetings">
-                                <Card>
-                                    <Card.Header style={{marginBottom:"px"}} className="gd-card-header" >
-                                        {item.supervisor}
-                                        <div className="row">
-                                            <Col md={2} className="time">
-                                                {item.date.substring(0, 10)}
-                                            </Col>
-                                            <Col md={6} className="time">
-                                                {item.time}
-                                            </Col>
-                                        </div>
-                                    </Card.Header>
-                                    <Card.Body>
-                                        <p className="vm-purpose">{item.purpose}</p>
-                                    </Card.Body>
-                                </Card>
-                            </div>
-                        </Col>
-                    </div>
-                )
-            })
-        return (
-            <div className="row" style={{ marginBottom :"20px"}}>
-                {supervisorProjectList}
-            </div>
-        );
+                  <tr
+                    className="as-table-row"
+                    key={item.id}
+                    onClick={() => this.groupDataHandler(item.id)}
+                  >
+                    <td className="table-body">{item.supervisor}</td>
 
-    }
+                    <td className="table-body">{item.date.substring(0, 10)}</td>
+
+                    <td className="table-body">{item.time}</td>
+
+                    <td className="table-body">{item.purpose}</td>
+
+                    {item.state === "pending" && (
+                      <td className="table-body" style={{ color: "orange" }}>
+                        {item.state}
+                      </td>
+                    )}
+
+                    {item.state === "confirmed" && (
+                      <td className="table-body" style={{ color: "green" }}>
+                        {item.state}
+                      </td>
+                    )}
+                  </tr>
+                );
+              })}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
 
 export default viewMeetBlock;
