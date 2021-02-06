5ce7e937f3faab2ff47d0e8a96925e185ec6f723
nipuni-d <nipuniwarakagoda97@gmail.com>
2021-02-06 13:21:58 +0530

    Testing-ViewProject and StudentDetails

---
+++ frontend/cypress/integration/user/Student/StudentDetails.spec.js
@@ -0,0 +1,35 @@
+describe("Test Case 1 : StudentDetails", () => {
+  //   beforeEach(() => {
+  //     cy.visit("http://localhost:3000/");
+  //     cy.get("form").find("input").first().type("sachin@gmail.com");
+  //     cy.get("form").find("input").last().type("950880333v");
+  //     cy.get("form").contains("Login").click();
+  //     cy.contains("Login")
+  //       .click()
+  //       .then(() => {
+  //         cy.url().should("eq", "http://localhost:3000/" + "studenthome");
+  //       });
+  //   });
+
+  it("Login Student", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("form").find("input").first().type("sachin@gmail.com");
+    cy.get("form").find("input").last().type("950880333v");
+    cy.get("form").contains("Login").click();
+    cy.contains("Login")
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/" + "studenthome");
+      });
+  });
+
+  it("Student Details", () => {
+    cy.visit("http://localhost:3000/studenthome");
+    cy.contains("E-supervision")
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/studenthome/viewproject");
+      });
+    cy.contains("17000001").click();
+  });
+});
---
+++ frontend/cypress/integration/user/Student/ViewProject.spec.js
@@ -0,0 +1,34 @@
+describe("Test Case 1 : ViewProject", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+    cy.get("form").find("input").first().type("sachin@gmail.com");
+    cy.get("form").find("input").last().type("950880333v");
+    cy.get("form").contains("Login").click();
+    cy.contains("Login")
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/" + "studenthome");
+      });
+  });
+
+  it("Login Student", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("form").find("input").first().type("sachin@gmail.com");
+    cy.get("form").find("input").last().type("950880333v");
+    cy.get("form").contains("Login").click();
+    cy.contains("Login")
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/" + "studenthome");
+      });
+  });
+
+  it("View Project", () => {
+    cy.visit("http://localhost:3000/studenthome");
+    cy.contains("E-supervision")
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/studenthome/viewproject");
+      });
+  });
+});
