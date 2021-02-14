f94b7c6e33e343e60a22f69cf842b67a4b385402
nipuni-d <nipuniwarakagoda97@gmail.com>
2021-02-14 13:57:48 +0530

    Testing-Progress and BiweeklyReports

---
+++ frontend/cypress/integration/user/Student/Progress.spec.js
@@ -0,0 +1,42 @@
+describe("Test Case 1 : Progress", () => {
+  //   beforeEach(() => {
+  //     cy.visit("http://localhost:3000/");
+  //     cy.get("form").find("input").first().type("sachin@gmail.com");
+  //     cy.get("form").find("input").last().type("950880333v");
+  //     cy.get("form").contains("Login").click();
+  //     cy.contains("Login")
+  //       .click()
+  //       .then(() => {
+  //         cy.url().should("eq", "http://localhost:3000/" + "studenthome");
+  //       });
+  //   });
+
+  it("Login Student", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("form").find("input").first().type("sachin@gmail.com");
+    cy.get("form").find("input").last().type("950880333v");
+    cy.get("form").contains("Login").click();
+    cy.contains("Login")
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/" + "studenthome");
+      });
+  });
+
+  it("Progress", () => {
+    cy.visit("http://localhost:3000/studenthome");
+    cy.contains("E-supervision")
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/studenthome/viewproject");
+      });
+    cy.contains("Progress")
+      .click()
+      .then(() => {
+        cy.url().should(
+          "eq",
+          "http://localhost:3000/studenthome/viewproject/progresstasks"
+        );
+      });
+  });
+});
