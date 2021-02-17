5d1371f03a89a893c0727b0f60df7c01a39adcef
nipuni-d <nipuniwarakagoda97@gmail.com>
2021-02-17 13:40:11 +0530

    Testing - ViewFeedbacks and EditDetails

---
+++ frontend/cypress/integration/user/Student/EditDetails.spec.js
@@ -0,0 +1,40 @@
+describe("Test Case 1 : EditDetails", () => {
+  //   beforeEach(() => {
+  //     cy.visit("http://localhost:3000/");
+  //     cy.get("form").find("input").first().type("sachin@gmail.com");
+  //     cy.get("form").find("input").last().type("950880333v");
+  //     cy.get("form").contains("Login").click();
+  //     cy.contains("Login")
+  //       .click()
+  //       .then(() => {
+  //         cy.url().should("eq", "http://localhost:3000/" + "studenthome");
+  //       });
+  //   });
+
+  it("Login Student", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("form").find("input").first().type("sachin@gmail.com");
+    cy.get("form").find("input").last().type("950880333v");
+    cy.get("form").contains("Login").click();
+    cy.contains("Login")
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/" + "studenthome");
+      });
+  });
+
+  it("Edit Details", () => {
+    cy.visit("http://localhost:3000/studenthome");
+    cy.contains("E-supervision")
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/studenthome/viewproject");
+      });
+    cy.get("span.edit-span")
+      // .last()
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/studenthome/viewproject");
+      });
+  });
+});
---
+++ frontend/cypress/integration/user/Student/ViewFeedbacks.spec.js
@@ -0,0 +1,40 @@
+describe("Test Case 1 : ViewFeedbacks", () => {
+  //   beforeEach(() => {
+  //     cy.visit("http://localhost:3000/");
+  //     cy.get("form").find("input").first().type("sachin@gmail.com");
+  //     cy.get("form").find("input").last().type("950880333v");
+  //     cy.get("form").contains("Login").click();
+  //     cy.contains("Login")
+  //       .click()
+  //       .then(() => {
+  //         cy.url().should("eq", "http://localhost:3000/" + "studenthome");
+  //       });
+  //   });
+
+  it("Login Student", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("form").find("input").first().type("sachin@gmail.com");
+    cy.get("form").find("input").last().type("950880333v");
+    cy.get("form").contains("Login").click();
+    cy.contains("Login")
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/" + "studenthome");
+      });
+  });
+
+  it("View Feedbacks", () => {
+    cy.visit("http://localhost:3000/studenthome");
+    cy.contains("E-supervision")
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/studenthome/viewproject");
+      });
+    cy.get("span.btn-title")
+      .last()
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/studenthome/viewFeedback");
+      });
+  });
+});
