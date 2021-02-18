dc1209324d2ea4e2df98f83bd1e41f61c5a94d30
Maryamsuraiya <suraiyamaryam11@gmail.com>
2021-02-17 23:08:11 -0800

    Testing - Update Users

--- frontend/cypress/integration/user/Admin/UpdateUsers.spec.js
+++ frontend/cypress/integration/user/Admin/UpdateUsers.spec.js
@@ -1,5 +1,5 @@
 describe("Test Case 11: Update Users", () => {
-  it("Login Admin", () => {
+  beforeEach(() => {
     cy.visit("http://localhost:3000/");
     cy.get("form").find("input").first().type("3rdyeargroupproject0@gmail.com");
     cy.get("form").find("input").last().type("admin123");
@@ -28,6 +28,39 @@ describe("Test Case 11: Update Users", () => {
               "eq",
               "http://localhost:3000/editprofile/5eb7a9217ff4246104d8320f"
             );
+            cy.contains("Update User");
+            // cy.get("input[name=contactNumber]").type("0758693212");
+            cy.get("input").last().type("0758693212");
+            cy.contains("Confirm to reset");
+            cy.contains("Yes").click();
+          });
+      });
+  });
+
+  it("Reset Password", () => {
+    cy.visit("http://localhost:3000/adminhome");
+    cy.contains("Users").click();
+    cy.contains("Manage Users")
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/adminhome/viewusers");
+        cy.contains("List of Staff");
+        cy.contains("Actions");
+        cy.get("svg.MuiSvgIcon-root.edit-btn")
+          .first()
+          .click()
+          .then(() => {
+            cy.url().should(
+              "eq",
+              "http://localhost:3000/editprofile/5eb7a9217ff4246104d8320f"
+            );
+            cy.contains("Reset Password");
+            // cy.get("input[name=contactNumber]").type("0758693212");
+            // cy.get("input").last().type("0758693212");
+            // cy.contains("Confirm to reset");
+            cy.contains("Reset password").click();
+            cy.contains("Confirm to reset");
+            cy.contains("Yes").click();
           });
       });
   });
