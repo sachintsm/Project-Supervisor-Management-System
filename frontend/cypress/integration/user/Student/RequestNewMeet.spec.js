b48367d6ec1e1fa9541492e953b7e7a04fa31f85
nipuni-d <nipuniwarakagoda97@gmail.com>
2021-03-05 12:05:08 +0530

    Testing-Functions of the student panel

---
+++ frontend/cypress/integration/user/Student/RequestNewMeet.spec.js
@@ -0,0 +1,44 @@
+describe("Test Case 1 : RequestNewMeet", () => {
+  //   beforeEach(() => {
+  //     cy.visit("http://localhost:3000/");
+  //     cy.get("form").find("input").first().type("sachin@gmail.com");
+  //     cy.get("form").find("input").last().type("950880333v");
+  //     cy.get("form").contains("Login").click();
+  //     cy.contains("Login")
+  //       .click()
+  //       .then(() => {
+  //         cy.url().should("eq", "http://localhost:3000/" + "studenthome");
+  //       });
+  //   });
+
+  it("Login Student", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("form").find("input").first().type("sachin@gmail.com");
+    cy.get("form").find("input").last().type("950880333v");
+    cy.get("form").contains("Login").click();
+    cy.contains("Login")
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/" + "studenthome");
+      });
+  });
+
+  it("Request New Meet", () => {
+    cy.visit("http://localhost:3000/studenthome");
+    cy.contains("E-supervision")
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/studenthome/viewproject");
+      });
+    cy.contains("Meetings")
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/student/viewMeeting");
+      });
+    cy.contains("New Meet")
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/student/viewMeeting");
+      });
+  });
+});
