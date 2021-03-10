4f76d76ac2c2f9ea40a7c27d9bd1d0ab04bb02b2
Maryamsuraiya <suraiyamaryam11@gmail.com>
2021-03-10 06:22:04 -0800

    coodinator profile testing

---
+++ frontend/cypress/integration/user/Coordinator/Profile.spec.js
@@ -0,0 +1,24 @@
+describe("Test Case 18: Profile", () => {
+  it("Login Coordinator", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get("form").find("input").first().type("shayanmalinda@gmail.com");
+    cy.get("form").find("input").last().type("970822151v");
+    cy.get("form").contains("Login").click();
+    cy.contains("Login")
+      .click()
+      .then(() => {
+        cy.url().should("eq", "http://localhost:3000/" + "coordinatorhome");
+      });
+  });
+
+  // it("Profile Settings", () => {
+  //   cy.visit("http://localhost:3000/coordinatorhome");
+  //   cy.get("mui-64144").click();
+  //   cy.contains("Settings")
+  //     // .first()
+  //     .click()
+  //     .then(() => {
+  //       cy.url().should("eq", "http://localhost:3000/profile");
+  //     });
+  // });
+});
